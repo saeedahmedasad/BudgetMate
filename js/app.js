41775e99d3eded6aecbc3fbfc8d9148a4b4bad1e
Saeed Ahmed <phoolisahab@gmail.com>
2023-03-30 17:47:10 +0500

    Add Responsive to Sidebar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const closeSidebarBtn = document.querySelector(".close")
+const openSidebarBtn = document.querySelector(".open")
+const sidebar = document.querySelector(".sidebar")
+
+closeSidebarBtn.addEventListener("click", ()=>{
+    sidebar.style.left = "-100%";
+    sidebar.style.overflow = "hidden"
+    openSidebarBtn.style.opacity = "1"
+})
+openSidebarBtn.addEventListener("click", ()=>{
+    sidebar.style.left = "5px";
+    sidebar.style.overflow = "visible"
+    openSidebarBtn.style.opacity = "0"
+})
